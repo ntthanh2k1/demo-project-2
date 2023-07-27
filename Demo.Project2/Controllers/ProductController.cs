d6499f5218537f493e566f18a781d73e2982ebd3
Trí Thanh Nguyễn <nguyentrithanh1311@gmail.com>
2023-07-27 11:37:06 +0700

    commit.

---
+++ Demo.Project2/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Demo.Project2.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Project2.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        private readonly DemoProject2DbContext _context;
+
+        public ProductController(DemoProject2DbContext context)
+        {
+            _context = context;
+        }
+
+        #region Xem chi tiết sản phẩm
+        [HttpGet]
+        [Route("details/{id}")]
+        public async Task <IActionResult> Details(Guid id)
+        {
+            var product = await _context.Products!.FirstOrDefaultAsync(a => a.Id.Equals(id));
+            return View("details", product);
+        }
+        #endregion Xem chi tiết sản phẩm
+    }
+}
