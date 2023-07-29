9cca8afbaf6c1da4f8dcc617fa8583b5624f9ea4
ThanhNG1311 <nguyentrithanh1311@gmail.com>
2023-07-30 04:48:12 +0700

    commit.

--- Demo.Project2/Controllers/ProfileController.cs
+++ Demo.Project2/Controllers/ProfileController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Demo.Project2.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Demo.Project2.Controllers
 {
+    [Authorize(Roles = "Customer", AuthenticationSchemes = "CustomerSchemes")]
+    [Route("profile")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly DemoProject2DbContext _context;
+
+        public ProfileController(DemoProject2DbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        #region Trang thông tin cá nhân
+        [HttpGet]
+        [Route("")]
+        [Route("index")]
+        public async Task<IActionResult> Index()
+        {
+            var id = User.FindFirstValue(ClaimTypes.Sid);
+            var user = await _context.Users!.FirstOrDefaultAsync(a => a.Id.Equals(Guid.Parse(id)));
+            return View(user);
+        }
+        #endregion Trang thông tin cá nhân
     }
 }
